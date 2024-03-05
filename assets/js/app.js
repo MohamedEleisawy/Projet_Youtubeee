52410017f80692745285c1bfa732ee42e20fe63e
Mohamed_Eleisawy <135170480+MohamedEleisawy@users.noreply.github.com>
2024-03-05 09:24:58 +0100

    ddd

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,13 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", ()=>{
-
+    let accordion = document.getElementById('accordion');
+    let ouvertureDesMentionsLegales = document.getElementById('ouvertureDesMentionsLegales');
+    
+    ouvertureDesMentionsLegales.addEventListener('click', function () {
+        if (accordion.style.display === 'block') {
+            accordion.style.display = 'none';
+        } else {
+            accordion.style.display = 'block';
+        }
+    });
 });
