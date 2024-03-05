87a0807d8f25963fc49185a08dd4470489e9399c
Mohamed_Eleisawy <135170480+MohamedEleisawy@users.noreply.github.com>
2024-03-05 11:34:36 +0100

    mmm

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,21 @@
-'use strict';
 document.addEventListener("DOMContentLoaded", ()=>{
-    let accordion = document.getElementById('accordion');
-    let ouvertureDesMentionsLegales = document.getElementById('ouvertureDesMentionsLegales');
+    'use strict';
+    // let accordion = document.getElementById('accordion');
+    // let ouvertureDesMentionsLegales = document.getElementById('ouvertureDesMentionsLegales');
     
-    ouvertureDesMentionsLegales.addEventListener('click', function () {
-        if (accordion.style.display === 'block') {
-            accordion.style.display = 'none';
-        } else {
-            accordion.style.display = 'block';
-        }
+    // ouvertureDesMentionsLegales.addEventListener('click', function () {
+    //     if (accordion.style.display === 'block') {
+    //         accordion.style.display = 'none';
+    //     } else {
+    //         accordion.style.display = 'block';
+    //     }
+    // });
+    let btnHamburger = document.getElementById('hamburger');
+    let menu = document.getElementById('menu');
+    
+    btnHamburger.addEventListener('click', function() {
+      menu.classList.toggle('show');
     });
+    
+      
 });
